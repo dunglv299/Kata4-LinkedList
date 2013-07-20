9a489ce6e8ff3c256fa90686f2859aec97c74c60
dunglv299 <dunglv90@gmail.com>
2013-07-20 14:18:38 +0700

    test3: green

--- src/SingleLinkedListTest.java
+++ src/SingleLinkedListTest.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class SingleLinkedListTest {
-	// step 1
+	// step 2
 	@Test
 	public void testCreateNewSingleLinkedList() {
 		MySingleLinkedList linkedList = new MySingleLinkedList();
 		assertTrue(linkedList != null);
+		assertEquals(0, linkedList.getListSize());
+	}
+
+	// step 3
+	@Test
+	public void testGetSizeOfList() {
+		MySingleLinkedList linkedList = new MySingleLinkedList();
 		assertEquals(0, linkedList.getListSize());
 	}
 }
